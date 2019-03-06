d8324fada927d0f7bcba166d3bef7a4e8e42dac2
takenokoya <takuya1002@gmail.com>
2019-03-06 20:09:34 +0900

    計算アプリを再度修正(浮動小数点演算)

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -4,16 +4,16 @@ class CalcsController < ApplicationController
     @value1 = params[:value1].to_i  # params[XXX]で受け取った文字列を数字に変換して変数に代入
     @value2 = params[:value2].to_i
     @operator = params[:operator]
-
-    @result = case @operator  #case文を用いてparams[:operator]で受け取る値に応じた処理を実施
+    # コードを簡素化
+    @result = case @operator  #case文を用いてparams[:operator]で受け取る値に応じた処理を@resultに代入
               when 'addition'
-                @value1 + @value2
+                @value1 + @value2 
               when 'subtraction'
                 @value1 - @value2
               when 'multiplication'
                 @value1 * @value2
               when 'division'
-                @value1 / @value2
+                @value1 / @value2.to_f  #浮動小数点演算に修正
               end
   end
 
