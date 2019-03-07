84959a4c919969d939874e2ae11eca57d7983091
takenokoya <takuya1002@gmail.com>
2019-03-07 13:07:26 +0900

    計算アプリの拡充(ローカル変数に置き換えの微修正)

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -5,21 +5,21 @@ module Common
   extend ActiveSupport::Concern
 
   def calculation
-    @value1 = params[:value1].to_i  # params[XXX]で受け取った文字列を数字に変換して変数に代入
-    @value2 = params[:value2].to_i
-    @operator = params[:operator]
-    @result = case @operator  #case文を用いてparams[:operator]で受け取る値に応じた処理を@resultに代入
+    value1 = params[:value1].to_i  # params[XXX]で受け取った文字列を数字に変換してローカル変数に代入
+    value2 = params[:value2].to_i
+    operator = params[:operator]
+    @result = case operator  #case文を用いてparams[:operator]で受け取る値に応じた処理を@resultに代入
               when 'addition'
-                @value1 + @value2
+                value1 + value2
               when 'subtraction'
-                @value1 - @value2
+                value1 - value2
               when 'multiplication'
-                @value1 * @value2
+                value1 * value2
               when 'division'
                 # 課題2:ZeroDivisionErrorをresqueを用いて補足し、変数eに例外オブジェクトを格納
                 # 例外が起きた場合は変数eを@resultに代入
                 begin
-                  "#{@value1 / @value2}..#{@value1 % @value2}" #課題1:余りも出力するよう追加
+                  "#{value1 / value2}..#{value1 % value2}" #課題1:余りも出力するよう追加
                 rescue ZeroDivisionError => e
                   e
                 end
