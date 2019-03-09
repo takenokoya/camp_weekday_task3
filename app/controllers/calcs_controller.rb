c1a374a6036d7819ff8d899cd6d867a9410c7068
takenokoya <takuya1002@gmail.com>
2019-03-09 20:10:39 +0900

    controllerとmoduleのリファクタリング

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -3,7 +3,10 @@ class CalcsController < ApplicationController
   include Common  
 
   def show
-     calculation  #Commonモジュールのculculationメソッドを呼び出す
+    value1 = params[:value1].to_i  # params[XXX]で受け取った文字列を数字に変換して変数に代入
+    value2 = params[:value2].to_i
+    operator = params[:operator]
+    @result = calculation(value1, operator, value2) #Commonモジュールのculculationメソッドを呼び出す
   end
 
 end
